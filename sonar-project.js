179264f26bda6016e03f8f9e75815c0d8b99ce06
soma23s <147039495+soma23s@users.noreply.github.com>
2023-11-14 14:29:31 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.234.75.115:9000/',
+  serverUrl: 'http://172.31.17.42:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_dd9c695804fe2d2f68846dc0fe99b20f7d906d88',
+	    'sonar.login': 'squ_a39e4a33dd42bb14504f74f26f4d82671df793c6',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
